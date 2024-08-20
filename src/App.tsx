9e25c369a08f6b40e2e5703bf0e58ee61e21eb56
Krish <prathamrajsri2003@gmail.com>
2024-08-20 21:59:34 +0530

    final commit maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-import { generateMnemonic } from "bip39"
+import { generateMnemonic } from "bip39";
+import { SolanaWallet } from './components/SolWallet';
+import { EthWallet } from './components/EthWallet';
+
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
 
-  
-  return <div>
-        <button onClick={async function() {
-        const mn = await generateMnemonic();
-        setMnemonic(mn)
-      }}>
-        Create Seed Phrase
-      </button>
+  return (
+    <>
+     <div className='flex justify-center items-center pt-20'>
       <input type="text" value={mnemonic}></input>
-  </div>
+        <button onClick={async function() {
+          const mn = generateMnemonic();
+          setMnemonic(mn)
+        }}>
+          Create Seed Phrase
+        </button>
+        {mnemonic && <SolanaWallet mnemonic={mnemonic} />}
+        {mnemonic && <EthWallet mnemonic={mnemonic} />}
+     </div>
+    </>
+  )
 }
 
 export default App
